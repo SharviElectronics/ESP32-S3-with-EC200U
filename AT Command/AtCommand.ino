a18b082bba0a84ad374a8c27c6b85653da487caf
Rajkumar181 <suryadevkuswaha@gmail.com>
2025-06-25 21:34:50 +0530

    Update AtCommand.ino

--- AT Command/AtCommand.ino
+++ AT Command/AtCommand.ino
@@ -1,11 +1,11 @@
-#define RXD2 18
-#define TXD2 17
+#define EC200U_RX   18
+#define EC200U_TX   17
 #define PW_KEY  10  
 
 void setup() {
   pinMode(PW_KEY, OUTPUT);  // Output for Power Key
   Serial.begin(115200);
-  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);  // Connect through UART
+  Serial2.begin(115200, SERIAL_8N1, EC200U_RX, EC200U_TX);  // Connect through UART
   digitalWrite(PW_KEY, LOW); // Enable the EC200U by Power Key
 
 }
